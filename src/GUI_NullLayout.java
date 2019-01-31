908a3fcf01705a6713b234c12864ae3d3d85424f
Benjamin Viramontes <benja120599@gmail.com>
2019-01-31 11:31:53 -0600

    Se agregó "label2"

--- src/GUI_NullLayout.java
+++ src/GUI_NullLayout.java
@@ -15,12 +15,29 @@ class VentanaPrincipal extends JFrame {
 		setVisible(true);
 		
 		Font Titulo = new Font("Arial", Font.PLAIN, 12);
-		Font Texto = new Font("Arial", Font.PLAIN, 12);
 		
-		JLabel label1 = new JLabel("<html><p>The <b>Classic Form</b> to includes all visible fields for\rthis list</p></html>");
+		JLabel label1 = new JLabel("<html><p>The <b>Classic Form</b> to includes all visible fields for this list.</p></html>");
 			label1.setBounds(20, 15, 280, 30);
 			label1.setFont(Titulo);
 		add(label1);
+		
+		JLabel label2 = new JLabel("<html><p>Form options</p></html>");
+			label2.setBounds(20, 50, 280, 30);
+			label2.setFont(new Font("Arial", Font.BOLD, 13));
+		add(label2);
+		
+		JLabel label3 = new JLabel("<html><p>Include the following:</p></html>");
+			label3.setBounds(20, 75, 280, 30);
+			label3.setFont(new Font("Arial", Font.PLAIN, 12));
+		add(label3);
+		
+		JCheckBox checkBox1 = new JCheckBox("A title for you form.");
+			checkBox1.setBounds(15, 100, 280, 50);
+			checkBox1.setFont(Titulo);
+			checkBox1.setBorderPainted(false);
+			checkBox1.setBackground(Color.WHITE);
+		add(checkBox1);
+		
 	}
 	
 	public void configurar() {
