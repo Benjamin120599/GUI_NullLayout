432888f818cc77daccc5cfd8b6d75e3de2c78cee
Benjamin Viramontes <benja120599@gmail.com>
2019-01-31 13:03:07 -0600

    Se añadió "label9"

--- src/GUI_NullLayout.java
+++ src/GUI_NullLayout.java
@@ -44,84 +44,89 @@ class VentanaPrincipal extends JFrame {
 		ButtonGroup bg = new ButtonGroup();
 		
 		JRadioButton radioButton1 = new JRadioButton("   Only required fields.");
-			radioButton1.setBounds(15, 190, 280, 30);
+			radioButton1.setBounds(15, 190, 280, 20);
 			radioButton1.setFont(Titulo);
 			radioButton1.setBackground(Color.WHITE);
 		bg.add(radioButton1);
 		add(radioButton1);
 		
 		JRadioButton radioButton2 = new JRadioButton("   All fields.");
-			radioButton2.setBounds(15, 215, 280, 30);
+			radioButton2.setBounds(15, 210, 280, 20);
 			radioButton2.setFont(Titulo);
 			radioButton2.setBackground(Color.WHITE);
 		bg.add(radioButton2);
 		add(radioButton2);
 		
 		JLabel label4 = new JLabel("(Edit required fields in");
-			label4.setBounds(40, 235, 280, 30);
+			label4.setBounds(40, 225, 280, 30);
 			label4.setFont(new Font("Arial", Font.PLAIN, 10));
 		add(label4);
 		
 		JLabel label5 = new JLabel("the form builder");
-			label5.setBounds(150, 235, 280, 30);
+			label5.setBounds(150, 225, 280, 30);
 			label5.setFont(new Font("Arial", Font.PLAIN, 10));
 			label5.setForeground(Color.BLUE);
 		add(label5);
 		
 		JLabel label6 = new JLabel(")");
-			label6.setBounds(225, 235, 280, 30);
+			label6.setBounds(225, 225, 280, 30);
 			label6.setFont(new Font("Arial", Font.PLAIN, 10));
 		add(label6);
 		
 		JCheckBox checkBox2 = new JCheckBox("   Interest group field.");
-			checkBox2.setBounds(15, 280, 280, 30);
+			checkBox2.setBounds(15, 260, 280, 30);
 			checkBox2.setFont(Titulo);
 			checkBox2.setBackground(Color.WHITE);
 		add(checkBox2);
 		
 		JCheckBox checkBox3 = new JCheckBox("   Required Fields Indicators.");
-			checkBox3.setBounds(15, 305, 280, 30);
+			checkBox3.setBounds(15, 285, 280, 30);
 			checkBox3.setFont(Titulo);
 			checkBox3.setBackground(Color.WHITE);
 		add(checkBox3);
 		
 		JLabel label7 = new JLabel("Set form width,");
-			label7.setBounds(20, 340, 280, 30);
+			label7.setBounds(20, 320, 280, 30);
 			label7.setFont(Titulo);
 		add(label7);
 		
 		JTextField textField2 = new JTextField();
-			textField2.setBounds(18, 370, 270, 30);
+			textField2.setBounds(18, 350, 270, 30);
 		add(textField2);
 		
 		JLabel label8 = new JLabel("Enhace your form");
-			label8.setBounds(20, 410, 280, 30);
+			label8.setBounds(20, 390, 280, 30);
 			label8.setFont(Titulo);
 		add(label8);
 		
 		JCheckBox checkBox4 = new JCheckBox("<html>   Enable <b>evil</b> popup mode.</html>");
-			checkBox4.setBounds(15, 430, 280, 30);
+			checkBox4.setBounds(15, 410, 280, 30);
 			checkBox4.setFont(Titulo);
 			checkBox4.setBackground(Color.WHITE);
 		add(checkBox4);
 	
 		JCheckBox checkBox5 = new JCheckBox("<html>   Disable all <b>JavaScript</b>.</html>");
-			checkBox5.setBounds(15, 455, 280, 30);
+			checkBox5.setBounds(15, 435, 280, 30);
 			checkBox5.setFont(Titulo);
 			checkBox5.setBackground(Color.WHITE);
 		add(checkBox5);
 
 		JCheckBox checkBox6 = new JCheckBox("<html>   Include archive link.</html>");
-			checkBox6.setBounds(15, 480, 280, 30);
+			checkBox6.setBounds(15, 460, 280, 30);
 			checkBox6.setFont(Titulo);
 			checkBox6.setBackground(Color.WHITE);
 		add(checkBox6);
 
 		JCheckBox checkBox7 = new JCheckBox("<html>   Include MonkeyRewards link.</html>");
-			checkBox7.setBounds(15, 505, 280, 30);
+			checkBox7.setBounds(15, 485, 280, 30);
 			checkBox7.setFont(Titulo);
 			checkBox7.setBackground(Color.WHITE);
 		add(checkBox7);
+		
+		JLabel label9 = new JLabel("Preview");
+			label9.setBounds(310, 8, 280, 30);
+			label9.setFont(new Font("Arial", Font.BOLD, 14));
+		add(label9);
 	}
 	
 	public void configurar() {
